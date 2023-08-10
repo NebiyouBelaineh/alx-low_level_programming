df73eebe6152edceba21d73957e46d271ea0182d
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-08-10 23:51:32 +0300

    4. Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,11 @@
 #include <stdio.h>
-#include <string.h>
-int main(void)
-{	/*Prints string*/
-	puts("\"Programming is like building a multilingual puzzle\n");
 
-	return (0);
+/**
+ * main - Prints string using puts()
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
 }
