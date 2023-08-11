eb82037df14a65eab37ab6ad8f224346c5267710
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-08-11 23:21:23 +0300

    10. Inventing is a combination of brains and materials. The more brains you use, the less material you need

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,24 +12,24 @@ int main(void)
 	int first_d, second_d;
 
 	first_d = 0;
-	
-	for (;first_d <= 8; first_d++)
+
+	for (; first_d <= 8; first_d++)
 	{
 		for (second_d = first_d + 1; second_d <= 9; second_d++)
 		{
 			/*prints 1st digit*/
-				putchar((first_d % 10) + '0');
+			putchar((first_d % 10) + '0');
 			/*prints 2nd digit*/
-				putchar((second_d % 10) + '0');
+			putchar((second_d % 10) + '0');
 			if (first_d == 8 && second_d == 9)
 				continue;
 			/*prints comma*/
-				putchar(',');
+			putchar(',');
 			/*prints space*/
-				putchar(' ');
+			putchar(' ');
 		}
-		}
-	
+	}
+
 	putchar('\n');
 
 	return (0);
