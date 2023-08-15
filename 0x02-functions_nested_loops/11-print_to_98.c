1d70dfdc13a535ecc921c54f244c48424a0eb318
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-08-16 01:34:59 +0300

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -14,11 +14,11 @@ void print_to_98(int n)
 	{	int i;
 		for (i = n; i >= 98; i--)
 		{
-			printf("%d",i);
-			
-			if(i == 98)
+			printf("%d", i);
+
+			if (i == 98)
 				continue;
-                        else
+			else
 			{
 				printf(", ");
 			}
@@ -26,15 +26,15 @@ void print_to_98(int n)
 	}
 	else if (n == 98)
 	{
-		printf("%d",n);
+		printf("%d", n);
 	}
 	else
 	{	int i;
 		for (i = n; i <= 98; i++)
-		{	
+		{
 			printf("%d", i);
-		
-			if(i == 98)
+
+			if (i == 98)
 				continue;
 			else
 			{
