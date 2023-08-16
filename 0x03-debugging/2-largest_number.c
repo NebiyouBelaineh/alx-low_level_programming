2f1d6dbc622e8836f109459ef72d016b6d2325e7
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-08-17 02:13:33 +0300

    2. 0 > 972

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -16,17 +16,13 @@ int largest_number(int a, int b, int c)
 	{
 		largest = a;
 	}
-	else if (b > a && b > c)
+	else if (b > c)
 	{
 		largest = b;
 	}
-	else if (c > a && c > b)
+	else
 	{
 		largest = c;
-	}
-	else if (a == b && a == c)
-	{
-		largest = a;
 	}
 	return (largest);
 }
