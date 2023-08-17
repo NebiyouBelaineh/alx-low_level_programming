9d33eb00cb5f3dafca58edf3aae053cb89e4625f
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-08-17 20:01:36 +0300

    10. Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle of size n
+ * @n: size for triangle
+ * Return: void
+ */
+
+void print_triangle(int n)
+{
+	int i, j, k;
+
+	if (n > 0)
+	{
+		for (i = 0; i < n; i++)
+		{
+			for (j = n - 1 ; j > i; j--)
+			{
+				_putchar('-');
+			}
+			for (k = 0; k < i + 1; k ++)
+			{	
+				_putchar('#');
+			}
+			
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
