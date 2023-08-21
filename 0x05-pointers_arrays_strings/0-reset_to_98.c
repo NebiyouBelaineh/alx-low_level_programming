ebac0b5ef813b5c2346b45abc529dd5185f8efd8
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-08-21 21:45:17 +0300

    0. 98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * void reset_to_98 - updates the value it points to 98
+ * reset_to_98 - updates the value it points to 98
  * @n: pointer to variable
  *
  * Return: void
