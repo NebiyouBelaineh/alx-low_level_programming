b55211ed52d7a20ef4fe3ececf0a8d6316643d2f
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-08-24 01:03:49 +0300

    8. rot13

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * rot13 - encodes a string by rot13
+ * @s: string to be encoded by rot13
+ *
+ * Return: char after rot13 (s)
+ */
+char *rot13(char *s)
+{
+	int i, j;
+	char x[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char y[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; x[j] != '\0'; j++)
+		{
+			if (s[i] == x[j])
+			{
+				s[i] = y[j];
+				break;
+			}
+		}
+	}
+
+	return (s);
+}
