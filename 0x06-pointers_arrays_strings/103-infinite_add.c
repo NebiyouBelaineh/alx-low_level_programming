c19c1d7a972f26aa978bcc2736497640daf8e576
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-08-24 21:26:22 +0300

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -10,46 +10,33 @@
  */
 char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
-	int i, j, of, k, num1, num2, sum;
+	int i, j, of = 0, k = 0, num1, num2, sum;
 	char temp;
 
-	of = 0;
-	k = 0;
+	for (i = 0; n1[i] != '\0' || n2[j] != '\0'; i++, j++)
 
-	for (i = 0; n1[i] != '\0';)
-		i++;
-	for (j = 0; n2[j] != '\0';)
-		j++;
 	i--, j--;
-
 	for (; i >= 0 || j >= 0 || of == 1; i--, j--, k++)
 	{
 		if (i < 0)
 			num1 = 0;
 		else
 			num1 = n1[i] - '0';
-
 		if (j < 0)
 			num2 = 0;
 		else
 			num2 = n2[j] - '0';
-
 		sum = num1 + num2 + of;
-
 		if (sum > 9)
 			of = 1;
 		else
 			of = 0;
-
 		if (k >= size_r - 1)
 			return (0);
-
 		r[k] = (sum % 10) + '0';
 	}
-
 	if (k == size_r)
 		return (0);
-
 	r[k] = '\0';
 	for (i = 0; i < k / 2; i++)
 	{
@@ -57,6 +44,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		r[i] = r[k - 1 - i];
 		r[k - 1 - i] = temp;
 	}
-
 	return (r);
 }
