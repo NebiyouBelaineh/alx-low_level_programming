b21f78c26ed3082990b547e06f694b0233f0fb14
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-08-28 19:43:14 +0300

    2. strchr - simplify code by returning directly from loop

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 /**
  * _strchr - locates a character in a string
  * @s: string to be examined for first apperance of character 'c'
@@ -10,19 +9,12 @@
  */
 char *_strchr(char *s, char c)
 {
-	int i, flag;
-
-	flag = -1;
+	int i;
 
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (c == s[i])
-		{
-			flag = i;
-			break;
-		}
+			return ((s + i));
 	}
-	if (flag != (-1))
-		return ((s + flag));
-	return (NULL);
+	return ('\0');
 }
