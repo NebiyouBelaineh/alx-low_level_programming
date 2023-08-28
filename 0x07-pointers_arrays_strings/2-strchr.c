1bc4854cff87e0132ce9b05ce1346132e03c7b07
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-08-28 17:43:04 +0300

    2. strchr - solution

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * _strchr - locates a character in a string
+ * @s: string to be examined for first apperance of character 'c'
+ * @c: character of interest
+ *
+ * Return: 's' or NULL, depending on the occurrence or absence of
+ * 'c'
+ */
+char *_strchr(char *s, char c)
+{
+	int i, flag;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (c == s[i])
+		{
+			flag = i;
+			break;
+		}
+	}
+
+	return ((s + flag));
+}
