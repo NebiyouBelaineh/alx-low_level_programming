3a6f5adbdf11be83cc82ab4388f19229b969ea83
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-08-28 17:50:50 +0300

    2. strchr - fix NULL case

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stddef.h>
 /**
  * _strchr - locates a character in a string
  * @s: string to be examined for first apperance of character 'c'
@@ -11,6 +12,8 @@ char *_strchr(char *s, char c)
 {
 	int i, flag;
 
+	flag = -1;
+
 	for (i = 0; s[i] != '\0'; i++)
 	{
 		if (c == s[i])
@@ -19,6 +22,7 @@ char *_strchr(char *s, char c)
 			break;
 		}
 	}
-
-	return ((s + flag));
+	if (flag != (-1))
+		return ((s + flag));
+	return (NULL);
 }
