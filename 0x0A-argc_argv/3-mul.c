5d78bc90f76cd09a275cbe04596410c696dc29c5
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-09-01 23:34:44 +0300

    3. Neither irony nor sarcasm is argument

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -13,7 +13,7 @@ int main(int argc, char *argv[])
 
 	if (argc > 1)
 	{
-		product = atoi(*(argv + 1)) * atoi(*(argv + 2));
+		product = atoi(argv[1]) * atoi(argv[2]);
 		printf("%d\n", product);
 		return (0);
 	}
