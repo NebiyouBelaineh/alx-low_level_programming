f3655fd3976f67beb18f051ae52d1121a9f9e4d2
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-09-01 23:54:38 +0300

    4. To infinity and beyond

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,25 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-	int i, sum = 0, num = 0;
+	int i, j, sum = 0, num = 0;
 
 	if (argc == 1)
 	{
 		printf("0\n");
 		return (0);
 	}
-	for (i = 1; i < argc && *argv[i] != '\0'; i++)
+	for (i = 1; i < argc; i++)
 	{
-		if (*argv[i] < '0' && *argv[i] > '9')
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
-			printf("Error\n");
-			return (1);
+			if (argv[i][j] < '0' || argv[i][j] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-
 		num =  atoi(argv[i]);
 		sum += num;
 	}
-
 	printf("%d\n", sum);
 	return (0);
 }
