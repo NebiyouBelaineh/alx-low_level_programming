7bd3d4d4610ccf9ed99f9fd2eeb89cc8c37a13c6
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-09-05 17:42:59 +0300

    Fix Memory Leak

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -7,12 +7,13 @@
  *
  * Return: Pointer to a 2D array, NULL on failure
  */
-int **alloc_grid(int width, int height)
+int **alloc_grid(int height, int width)
 {
 	int **ptr, i, j;
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
+
 	ptr = (int **)malloc(width * sizeof(int *));
 
 	if (ptr == NULL)
@@ -20,13 +21,12 @@ int **alloc_grid(int width, int height)
 		free(ptr);
 		return (NULL);
 	}
-
 	for (i = 0; i < width; i++)
 	{
 		ptr[i] = (int *)malloc(height * sizeof(int));
 		if (ptr[i] == NULL)
 		{
-			free(ptr);
+			free(ptr[i]);
 			return (NULL);
 		}
 	}
