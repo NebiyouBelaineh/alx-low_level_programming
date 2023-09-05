314d7317bc2d876db73c67d4e6431c698a6b4907
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-09-05 16:00:09 +0300

    3. If you even dream of beating me you'd better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -11,7 +11,7 @@ int **alloc_grid(int width, int height)
 {
 	int **ptr, i, j;
 
-	if (width <= 0 || height <= 0 ||)
+	if (width <= 0 || height <= 0)
 		return (NULL);
 	ptr = (int **)malloc(width * sizeof(int *));
 
