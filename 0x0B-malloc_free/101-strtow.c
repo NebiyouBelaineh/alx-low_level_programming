960d43a9455bf64b6c50482b847af54d95628058
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-09-07 00:58:48 +0300

    6. I will show you how great I am - Fix for 0 words, memory allocation and  leak

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,8 @@
+#include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
 #include <stddef.h>
+
 /**
  * find_length - find length of a string
  * @str: string paramenter
@@ -11,18 +13,37 @@ int find_length(char *str)
 {
 	int i = 0, len = 0;
 
-	while (str[i] != '\0')
+	while (str[i] != '\0' && str[i] != ' ')
+	{
+		len++;
+		i++;
+	}
+	return (len);
+}
+/**
+ * w_count - counts number of words separated by a space
+ * @str: string parameter
+ *
+ * Return: (int) number of words
+ */
+
+
+int w_count(char *str)
+{
+	int i, wc = 0, len = 0;
+
+	for (i = 0; str[i] != '\0'; i++)
+		len++;
+
+	for (i = 0; i < len; i++)
 	{
 		if (str[i] != ' ')
 		{
-			len++;
-			i++;
+			wc++;
+			i += find_length(str + i);
 		}
-		else if (str[i] == ' ')
-			i++;
 	}
-	len++;
-	return (len);
+	return (wc);
 }
 
 /**
@@ -34,43 +55,39 @@ int find_length(char *str)
 char **strtow(char *str)
 {
 	char **s;
-	int i = 0, j, k, l, len = 0, track = 0;
+	int i = 0, j, k, l, wc;
 
 	if (str == NULL || *str == '\0')
 		return (NULL);
-	len = find_length(str);
-	s = (char **)malloc(len * sizeof(char));
+
+	wc = w_count(str);
+	if (wc == 0)
+		return (NULL);
+
+	s = (char **)malloc((wc + 1) * sizeof(char *));
 	if (s == NULL)
 		return (NULL);
-	for (i = 0, l = 0; str[i] != '\0'; i++)
+	for (j = 0; j < wc; j++)
 	{
-		k = 0;
-		if (str[i] != ' ')
+		while (str[i] == ' ')
+			i++;
+		l = find_length(str + i);
+
+		s[j] = (char *)malloc((l + 1) * sizeof(char));
+
+		if (s[j] == NULL)
 		{
-			track = i;
-			while (str[track] != ' ')
-			{
-				k++;
-				track++;
-			}
-			s[l] = malloc((k + 1) * sizeof(char));
-			if (s[l] == NULL)
-			{
-				while (k--)
-					free(s[l]);
-				free(s);
-				return (NULL);
-			}
-			for (track = i, j = 0; j < k; j++)
-			{
-				s[l][j] = str[track];
-				track++;
-			}
-			i = track;
-			if (j == k)
-				l++;
+			for ( ; j >= 0; j--)
+				free(s[j]);
+			free(s);
+			return (NULL);
 		}
+		for (k = 0; k < l; k++)
+		{
+			s[j][k] = str[i++];
+		}
+		s[j][k] = '\0';
 	}
-	s[l] = NULL;
+	s[j] = NULL;
 	return (s);
 }
