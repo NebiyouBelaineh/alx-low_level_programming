854c546f706337feae04715ab119b72910417a3d
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-09-07 22:03:06 +0300

    3. array_range

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -7,7 +7,8 @@
  * @min:		1st element of the array
  * @max:		last element of the array
  *
- * Return:		Pointer to the newly created array.
+ * Return:		Pointer to the newly created array. If
+ *			min > max, return NULL.
  */
 int *array_range(int min, int max)
 {
@@ -22,8 +23,8 @@ int *array_range(int min, int max)
 	if (arr == NULL)
 		return (NULL);
 
-	for (i = min; i <= len; i++)
-		arr[i] = i;
+	for (i = 0; i <= len; i++)
+		arr[i] = min++;
 
 	return (arr);
 }
