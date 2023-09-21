c42d1bfc3e7e5bf12519166f868748607d52196d
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-09-22 01:30:21 +0300

    Fix code output

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,10 +9,9 @@
 
 void __attribute__((constructor)) print_first(void)
 {
-	char *str;
+	char *s;
 
-	str = "You're beat! and yet, you must allow,\n"
-		"I bore my house upon my back!\n ";
+	s = "You're beat! and yet, you must allow,\nI bore my house upon my back!\n";
 
-	printf("%s", str);
+	printf("%s", s);
 }
