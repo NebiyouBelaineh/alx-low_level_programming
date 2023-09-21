17c644ef279cd39b14ba9aafabd28c101ed223cf
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-09-22 00:02:20 +0300

    Add 100-first.c and lists.h for task 5. The Hare and the Tortoise

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ * print_first -	prints text before main function is executed
+ *
+ * Return: void.
+ */
+
+
+void __attribute__((constructor)) print_first()
+{
+	char *str;
+
+	str = "You're beat! and yet, you must allow,\n"
+		"I bore my house upon my back!\n ";
+
+	printf("%s", str);
+}
