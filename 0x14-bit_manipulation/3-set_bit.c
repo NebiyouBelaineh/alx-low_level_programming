98c7cf39ac088935cd931e5fb5ed818f1ec18557
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-09-29 17:55:04 +0300

    Add 3-set_bit.c and main.h for task 3.11

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * set_bit - sets the value of a bit to 1 at a given index
+ *
+ * @n: address of the number we want to manupliate the bit at index position
+ * @index: position of the number we want to manupliate
+ *
+ * Return: 1 for success or -1 if an error occurs
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int bin_num = (sizeof(*(n)) * 11);
+
+	if (index < bin_num)
+	{
+		*(n) = ((1 << index) | *(n));
+		return (1);
+	}
+	return (-1);
+}
