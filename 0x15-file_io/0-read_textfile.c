c4c7fb4bdf75da9f5fbb4031248a0f46e0dfcdf2
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-10-02 22:27:25 +0300

    Add 0-read_textfile.c and main.h for task 0. 0. Tread lightly, she is nearwq

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * read_textfile - reads a text file and prints to Standard output
+ *
+ * @filename: string containing the file name to be read
+ * @letters: number of letters we want to read
+ *
+ * Return: the number of letters that could be read and printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	ssize_t count_read = 0, count_write = 0;
+	char buffer[2048];
+
+	if (filename == NULL)
+		return (0);
+
+	fd = open(filename, O_RDONLY);
+
+	if (fd == -1)
+		return (0);
+
+	count_read = read(fd, buffer, sizeof(buffer));
+
+	if (count_read == -1)
+		return (0);
+
+	count_write = write(STDOUT_FILENO, buffer, letters);
+
+	if (count_write < (ssize_t)letters || count_write == -1)
+		return (0);
+
+	return (count_write);
+
+}
