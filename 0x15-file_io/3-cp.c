74dad7ca844a977837bf5768a5f1881ab025daab
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-10-03 05:09:11 +0300

    Fix buffer writing

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -9,34 +9,30 @@
  */
 int main(int argc, char *argv[])
 {
-	int count_write = 0, file_from, file_to, count_read = 0;
-	char buffer[1024];
+	int file_from, file_to;
+	ssize_t chrs, count_write;
+	char buf[1024];
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
 		exit(97);
 	}
 	file_from = open(argv[1], O_RDONLY);
-	count_read = read(file_from, buffer, sizeof(buffer));
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
+	error_handler(file_from, file_to, argv);
 
-	if (file_from == -1 || count_read == -1)
+	chrs = 1024;
+	while (chrs == 1024)
 	{
-		close(file_from);
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
+		chrs = read(file_from, buf, 1024);
+		if (chrs == -1)
+			error_handler(-1, 0, argv);
+		count_write = write(file_to, buf, chrs);
+		if (count_write == -1)
+			error_handler(0, -1, argv);
 	}
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-	count_write = write(file_to, buffer, count_read);
-
-	if (file_to == -1 || count_write == -1)
-	{
-		close(file_to);
-		close(file_from);
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-		exit(99);
-	}
 	if (close(file_from) == -1)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't close fd %i", file_from);
@@ -48,4 +44,25 @@ int main(int argc, char *argv[])
 		exit(100);
 	}
 	return (0);
+}
+
+/**
+ * error_handler - checks if files can be opened.
+ * @file_from: file_from.
+ * @file_to: file_to.
+ * @argv: arguments vector.
+ * Return: no return.
+ */
+void error_handler(int file_from, int file_to, char *argv[])
+{
+	if (file_from == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	if (file_to == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
 }
