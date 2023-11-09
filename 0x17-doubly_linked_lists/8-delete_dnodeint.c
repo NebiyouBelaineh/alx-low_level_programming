fd91e2cd1d8aa607582f773fab5ca9fdd3225057
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-09 23:12:31 +0300

    Add edge cases for last node and first node deletion

--- 0x17-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x17-doubly_linked_lists/8-delete_dnodeint.c
@@ -12,15 +12,25 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 	unsigned int node_count = 0;
 	dlistint_t *current = *head;
 
+	if (head == NULL || *head == NULL)
+		return (-1);
+
 	while (current != NULL)
 	{
 		if (index == 0)
 		{
 			*head = (*head)->next;
+			if (*head != NULL)
+				(*head)->prev = NULL;
 			return (1);
 		}
 		if (node_count == index)
 		{
+			if (current->next == NULL)
+			{
+				(current->prev)->next = NULL;
+				return (1);
+			}
 			(current->next)->prev = current->prev;
 			(current->prev)->next = current->next;
 			return (1);
