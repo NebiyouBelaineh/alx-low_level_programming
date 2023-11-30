562e85870fb95284d7115dc26251f795d510e207
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-30 21:05:00 +0300

    Change malloc sizeof to pointer hash_node_t

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,6 +9,7 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
+	unsigned int index;
 
 	if (size == 0)
 		return (NULL);
@@ -18,6 +19,12 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_table->size = size;
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (new_table->array == NULL)
+		return (NULL);
+	for (index = 0; index < size; index++)
+		new_table->array = NULL;
 
 	return (new_table);
 }
