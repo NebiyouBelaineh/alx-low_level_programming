20e313a3463eab2bcb294cc092d5e852539b7f17
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-30 19:13:55 +0300

    Remove array memory allocation and initlization

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,7 +9,6 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-	unsigned int index;
 
 	if (size == 0)
 		return (NULL);
@@ -19,14 +18,5 @@ hash_table_t *hash_table_create(unsigned long int size)
 		return (NULL);
 
 	new_table->size = size;
-
-	new_table->array = malloc(sizeof(hash_node_t) * size);
-
-	if (new_table->array == NULL)
-		return (NULL);
-
-	for (index = 0; index < size; index++)
-		new_table->array[index] = NULL;
-
 	return (new_table);
 }
