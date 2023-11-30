07ef8b83a4bfc9012e007fd86625c1eacad1977e
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-30 20:19:58 +0300

    Remove size zero condition

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -9,14 +9,20 @@
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *new_table;
-
-	if (size == 0)
-		return (NULL);
+	unsigned int index;
 
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
 
 	new_table->size = size;
+
+	new_table->array = malloc(sizeof(hash_node_t) * size);
+
+	if (new_table->array == NULL)
+		return (NULL);
+	for (index = 0; index < size; index++)
+		new_table->array = NULL;
+
 	return (new_table);
 }
