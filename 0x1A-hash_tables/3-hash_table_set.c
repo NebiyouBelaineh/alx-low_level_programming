2e5a9344ee41472549dc444f9f01f9b982d59439
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-12-01 01:59:35 +0300

    Add solution for task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,45 +1,47 @@
 #include "hash_tables.h"
 hash_node_t *add_node(const char *key, const char *value);
 /**
-* hash_table_set - adds an element to the hash table
-* @ht: is the hash table where an element is to be added
-* @key: is the key for the element
-* @value: is the value associated with the key.
-*
-* Return: 1 for success, 0 for failure
-*/
+ * hash_table_set - adds an element to the hash table
+ * @ht: is the hash table where an element is to be added
+ * @key: is the key for the element
+ * @value: is the value associated with the key.
+ *
+ * Return: 1 for success, 0 for failure
+ */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index, size;
-	hash_node_t *new_node;
+	hash_node_t *new_node, *tmp;
 
 	if (strcmp("", key) == 0)
 		return (0);
 
 	size = ht->size;
 	index = key_index((unsigned char *)key, size);
-
+	tmp  = ht->array[index];
 	/*We need to check key exisits in hash table*/
-	if (ht->array[index] != NULL)
+	if (tmp != NULL)
 	{
-		while (ht->array[index] != NULL)
+		while (tmp != NULL)
 		{
-			if (strcmp(key, ht->array[index]->key) == 0)
+			if (strcmp(key, tmp->key) == 0)
 			{
-				free(ht->array[index]->value);
-				ht->array[index]->value = strdup(value);
+				free(tmp->value);
+				tmp->value = strdup(value);
 				return (1);
 			}
-			ht->array[index] = ht->array[index]->next;
+			tmp = tmp->next;
 		}
 	}
-
-	/*Add key/value pair if it does not exisit*/
-	new_node = add_node(key, value);
-	if (new_node == NULL)
-		return (0);
-	new_node->next = ht->array[index];
-	ht->array[index] = new_node;
+	else
+	{
+		/*Add key/value pair if it does not exisit*/
+		new_node = add_node(key, value);
+		if (new_node == NULL)
+			return (0);
+		new_node->next = ht->array[index];
+		ht->array[index] = new_node;
+	}
 
 	return (1);
 }
@@ -50,7 +52,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
  * @value: value the key corresponds to
  *
  * Return: address of the new node
-*/
+ */
 hash_node_t *add_node(const char *key, const char *value)
 {
 	hash_node_t *new_node;
