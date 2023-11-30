7a18abce23f0adfdae56299e0663bd29891400a5
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-12-01 01:46:41 +0300

    Add solution for task 3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,5 +1,5 @@
 #include "hash_tables.h"
-
+hash_node_t *add_node(const char *key, const char *value);
 /**
 * hash_table_set - adds an element to the hash table
 * @ht: is the hash table where an element is to be added
@@ -11,9 +11,7 @@
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
 	unsigned long int index, size;
-
-	hash_node_t *new_node = NULL;
-
+	hash_node_t *new_node;
 
 	if (strcmp("", key) == 0)
 		return (0);
@@ -21,29 +19,48 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	size = ht->size;
 	index = key_index((unsigned char *)key, size);
 
-	/*Check for collision(i.e there is already an element at index)*/
-	/*Then add new node at the head of the list*/
+	/*We need to check key exisits in hash table*/
 	if (ht->array[index] != NULL)
 	{
-		/*Case where the node exists and value is to be replaced*/
-		if (strcmp(key, ht->array[index]->key) == 0)
+		while (ht->array[index] != NULL)
 		{
-			free(ht->array[index]->value);
-			ht->array[index]->key = strdup(value);
-			return (1);
+			if (strcmp(key, ht->array[index]->key) == 0)
+			{
+				free(ht->array[index]->value);
+				ht->array[index]->value = strdup(value);
+				return (1);
+			}
+			ht->array[index] = ht->array[index]->next;
 		}
-		ht->array[index] = ht->array[index]->next;
 	}
 
-	/*Create a node at the index in the hash table array*/
-	new_node = malloc(sizeof(hash_node_t *));
+	/*Add key/value pair if it does not exisit*/
+	new_node = add_node(key, value);
 	if (new_node == NULL)
 		return (0);
-
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
 
 	return (1);
+}
+
+/**
+ * add_node - adds a node at the begining of a list
+ * @key: value for the key of the hash table
+ * @value: value the key corresponds to
+ *
+ * Return: address of the new node
+*/
+hash_node_t *add_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (NULL);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	return (new_node);
 }
