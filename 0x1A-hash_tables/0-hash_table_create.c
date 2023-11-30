430b42f262c3af866ae92aff2d55a816d21f39e6
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-11-30 22:18:56 +0300

    Fix missing array index

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,9 +11,6 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *new_table;
 	unsigned int index;
 
-	if (size == 0)
-		return (NULL);
-
 	new_table = malloc(sizeof(hash_table_t));
 	if (new_table == NULL)
 		return (NULL);
@@ -22,9 +19,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	new_table->array = malloc(sizeof(hash_node_t *) * size);
 
 	if (new_table->array == NULL)
+	{
+		free(new_table);
 		return (NULL);
+	}
+
 	for (index = 0; index < size; index++)
-		new_table->array = NULL;
+		new_table->array[index] = NULL;
 
 	return (new_table);
 }
