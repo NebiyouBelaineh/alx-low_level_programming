040872d9043c39eda47421f0a7ae4732ae944156
NebiyouBelaineh <nebiyouseifu2@gmail.com>
2023-12-01 03:01:30 +0300

    Add solution for task 4

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,25 @@
+#include "hash_tables.h"
+/**
+* hash_table_get - retrieves a value associated with a key
+* @ht: hash table
+* @key: key we are looking to find
+*
+* Return: The value associated with the key, or NULL if key was not found
+*/
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index, size;
+
+	if (ht == NULL || key == NULL)
+		return (NULL);
+
+	size = ht->size;
+	index = key_index((unsigned char *)key, size);
+
+	if (ht->array[index] != NULL)
+		if (strcmp(ht->array[index]->key, key) == 0)
+		{ /*Key exists*/
+			return (ht->array[index]->value);
+		}
+	return (NULL);
+}
